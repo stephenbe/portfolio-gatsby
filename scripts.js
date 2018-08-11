ff830f1c2e213affa873de17b5df4c1560cd5645
Stephen Bégay <stephenbe292+github@gmail.com>
2018-08-10 23:09:21 -0400

    Create homepage animation

--- scripts.js
+++ scripts.js
@@ -3,12 +3,20 @@ var box1 = document.getElementById("box1");
 var box2 = document.getElementById("box2");
 var box3 = document.getElementById("box3");
 var box4 = document.getElementById("box4");
+var box5 = document.getElementById("box5");
+var box = document.getElementById("box");
 var intro = new TimelineLite();
 
+var $portrait = document.querySelector(".header__portrait");
+var $portraitImg = document.querySelector(".header__img");
+var $banner = document.querySelector(".header__banner");
+var $introTitle = document.querySelector(".intro__title");
+var $introTCalltoaction = document.querySelector(".intro__calltoaction");
+var $introDescription = document.querySelector(".intro__description");
+var portraitOffsetTop = $portrait.getBoundingClientRect().top + window.scrollY;
+var portraitOffsetLeft = $portrait.getBoundingClientRect().left + window.scrollY;
+
 intro
-  //.to(boxintro, 0.4, {opacity:"1", delay:0.2})
-  //.to(boxintro, 0.4, {scale:0.1666})
-  //.to(boxintro, 0.4, {top:"0", left:"0"})
   .to(box1, 0, {opacity:"1", delay:0.8})
   .to(box1, 0.5, {scaleX:1 , ease: Expo.easeOut}, "firstshot")
   .to(box4, 0, {opacity:"1"}, "firstshot")
@@ -17,4 +25,15 @@ intro
   .to(box2, 0.5, {scaleY:1, ease: Expo.easeOut}, "secondshot-=0.15")
   .to(box3, 0, {opacity:"1"}, "secondshot-=0.15")
   .to(box3, 0.5, {scaleX:1 , ease: Expo.easeOut}, "secondshot-=0.15")
+  .to(box5, 0, {opacity:"1"}, "-=0.15")
+  .to(box5, 0.5, {scaleY:1 , ease: Expo.easeOut},"-=0.15")
+  .to(box, 0.5, {top:portraitOffsetTop, left: portraitOffsetLeft , ease: Expo.easeOut})
+  .to(box, 0, {opacity:0}, "portraitBlend")
+  .to($portrait, 0, {opacity:1}, "portraitBlend")
+  .to($portraitImg, 0.8, {top:0, ease: Expo.easeOut}, "pickaboo")
+  .to($banner, 0.8, {top:0, ease: Expo.easeOut}, "pickaboo")
+  .from($introTitle, 0.8, {opacity:0, top:10, ease: Expo.easeOut}, "pickaboo")
+  .from($introDescription, 0.8, {opacity:0, top:10, ease: Expo.easeOut}, "pickaboo+=0.2")
+  .from($introTCalltoaction, 1, {opacity:0, top:10, ease: Expo.easeOut}, "pickaboo+=0.4")
+
 
