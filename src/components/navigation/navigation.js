714fa78ef045ef8778ae3e94ca75316d946a36aa
Stephen B <sushicandypizza@gmail.com>
2019-08-06 22:12:28 -0400

    remove div that is not needed

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -2,21 +2,19 @@ import React from "react"
 import NavigationStyles from "./Navigation.module.css"
 
 export default () => (
-  <div>
-    <nav className={NavigationStyles.nav}>
-      <ul className={NavigationStyles.ul}>
-        <li>
-          <button className={NavigationStyles.link}>Articles</button>
-        </li>
-        <li>
-          <a className={NavigationStyles.link} href="https://www.google.com/">
-            Resume
-          </a>
-        </li>
-        <li>
-          <button className={NavigationStyles.link}>Contact</button>
-        </li>
-      </ul>
-    </nav>
-  </div>
+  <nav className={NavigationStyles.nav}>
+    <ul className={NavigationStyles.ul}>
+      <li>
+        <button className={NavigationStyles.link}>Articles</button>
+      </li>
+      <li>
+        <a className={NavigationStyles.link} href="https://www.google.com/">
+          Resume
+        </a>
+      </li>
+      <li>
+        <button className={NavigationStyles.link}>Contact</button>
+      </li>
+    </ul>
+  </nav>
 )
