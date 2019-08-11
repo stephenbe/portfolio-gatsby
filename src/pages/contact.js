f596a323d7a95ac4ca73fcaf83a41d0db2292d6b
Stephen B <sushicandypizza@gmail.com>
2019-08-10 21:58:51 -0400

    remove header that is not used

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
-import Header from "../components/header"
 import Container from "../components/container/container"
 
 export default () => (
   <Container>
-    <Header headerText="Contact" />
-    <p>Send us a message!</p>
     <Link to="./">go back Home</Link>
   </Container>
 )
