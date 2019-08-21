f692242032ef0c8b44bd4ef8320f139ad9689264
Stephen Bégay <sushicandypizza@gmail.com>
2019-08-21 10:56:19 -0400

    Add the first blog post form markdown taken from :
    https://www.gatsbyjs.org/blog/2017-07-19-creating-a-blog-with-gatsby/

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,27 @@
 module.exports = {
   /* Your site config here */
   plugins: [
+    "gatsby-plugin-catch-links",
+    "gatsby-plugin-react-helmet",
     {
       resolve: "gatsby-plugin-react-svg",
       options: {
         rule: {
           include: /assets/,
         },
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "pages",
+      },
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [],
       },
     },
   ],
