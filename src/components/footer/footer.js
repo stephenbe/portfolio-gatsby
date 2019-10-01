f4ce3c5427b3594783c31d1ffbce37c4c7a0ff5e
Stephen B <sushicandypizza@gmail.com>
2019-09-30 21:37:27 -0400

    Finish footer + random hero styles

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,20 +3,29 @@ import Section from "../section/section"
 import FooterStyles from "./footer.module.css"
 
 export default () => (
-  <Section sectionText="Contact">
-    <footer>
-      <a className={FooterStyles.link} href="mailto:stephenbe@live.ca">
-        stephenbe@live.ca
-      </a>
-      <a
-        className={FooterStyles.link}
-        href="https://www.linkedin.com/in/stephenbe/"
-      >
-        Linkedin
-      </a>
-      <a className={FooterStyles.link} href="https://twitter.com/stephenbefd">
-        Twitter
-      </a>
+  <Section sectionText="Contact" isEnd="true">
+    <footer className={FooterStyles.footer}>
+      <div className={FooterStyles.inner}>
+        <a className={FooterStyles.link} href="mailto:stephenbe@live.ca">
+          stephenbe@live.ca
+        </a>
+        <a
+          className={FooterStyles.link}
+          href="https://www.linkedin.com/in/stephenbe/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Linkedin
+        </a>
+        <a
+          className={FooterStyles.link}
+          href="https://twitter.com/stephenbefd"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Twitter
+        </a>
+      </div>
     </footer>
   </Section>
 )
